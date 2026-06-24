4aa351df8f43eac875e76e3957c9cf15f901c787
Rafael-Dpaula <rafaeldepaula939@gmail.com>
2026-06-24 03:02:54 -0300

    Finalização dos menus e funcionalidade dinamica do sistema

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from Models.ControleVoo import (
 
 aviaoSelecionado = None
 aeroportoSelecionado = None
-selecionadoLista = None
 centroSelecionado = None
 centros: list[(str, CentroOperacional)] = []
 aeroportos: list[Aeroporto._nome, Aeroporto] = []
@@ -23,31 +22,33 @@ controles: list[(str, ControleVoo)] = []
 avioes: list[(Aviao._identificador, Aviao)] = []
 planos: list[(PlanoVoo._destino, PlanoVoo)] = []
 
-obs = ControleVoo()
-carga = FactoryAviao.criarAviao(
-    tipo="Transporte",
-    identificador="BTR1123",
-    modelo="KC-390 Millennium",
-    tipoCarga="Eletrodomesticos",
-    pesoCarga=72.4,
-)
+# INSTÂNCIAS ESTÁTICAS
+# obs = ControleVoo()
+# carga = FactoryAviao.criarAviao(
+#     tipo="Transporte",
+#     identificador="BTR1123",
+#     modelo="KC-390 Millennium",
+#     tipoCarga="Eletrodomesticos",
+#     pesoCarga=72.4,
+# )
 
-aeroporto = Aeroporto("Aeroporto Internacional", "AERP12", "Passo Fundo | RS")
-pista = Pista(0)
-aeroporto.adicionarPista(pista)
-plataforma = Plataforma(0)
-plataforma.ocupar(carga)
-aeroporto.adicionarPlataforma(plataforma)
+# aeroporto = Aeroporto("Aeroporto Internacional", "AERP12", "Passo Fundo | RS")
+# pista = Pista(0)
+# aeroporto.adicionarPista(pista)
+# plataforma = Plataforma(0)
+# plataforma.ocupar(carga)
+# aeroporto.adicionarPlataforma(plataforma)
 
-controle = ControleVoo()
-plano = PlanoVoo("Passo Fundo|RS", "Curitiba|PR", "13:00", "17:30", 7000)
-centro = CentroOperacional(aeroporto, controle)
+# controle = ControleVoo()
+# plano = PlanoVoo("Passo Fundo|RS", "Curitiba|PR", "13:00", "17:30", 7000)
+# centro = CentroOperacional(aeroporto, controle)
+# controle.adicionarAeronave(carga)
 
-aeroportos.append((aeroporto._nome, aeroporto))
-controles.append(("Controle Principal", obs))
-centros.append(("Centro", centro))
-avioes.append((carga._identificador, carga))
-planos.append((plano._destino, plano))
+# aeroportos.append((aeroporto._nome, aeroporto))
+# controles.append(("Controle Principal", obs))
+# centros.append(("Centro", centro))
+# avioes.append((carga._identificador, carga))
+# planos.append((plano._destino, plano))
 
 
 def menuLista(lista, titulo="LISTA"):
@@ -131,6 +132,7 @@ def menuMostrar():
     elif mn == 1:
         if aeroportoSelecionado is not None:
             print(aeroportoSelecionado.__str__())
+            return menuMostrar()
         else:
             print("Nenhum aeroporto selecionado.")
             return menuMostrar()
@@ -201,6 +203,12 @@ def menuManipulacao():
     print(
         f"Aeroporto Selecionado: {"Não selecionado" if aeroportoSelecionado is None else f"{aeroportoSelecionado.nome} | {aeroportoSelecionado.codigo} | {aeroportoSelecionado.cidade}"}"
     )
+    if aeroportoSelecionado is not None and len(
+        aeroportoSelecionado._plataformas
+    ) >= len(aeroportoSelecionado._pistas):
+        print(
+            f"AVISO: a quantidade de pistas e plataformas podem gerar conflitos. recomenda-se pelo menos {len(aeroportoSelecionado._plataformas)+1} pistas para evitar problemas de tráfego."
+        )
     print("[1] Selecionar Aeroporto")
     print("[2] Adicionar Avião ao Aeroporto")
     print("[3] Remover Avião do Aeroporto")
@@ -221,13 +229,21 @@ def menuManipulacao():
             aeroportoSelecionado = resultado
         return menuManipulacao()
     elif mn == 2:
-        if centroSelecionado is None:
-            print("ALERT: primeiro selecione um aeroporto.")
+        if centroSelecionado is None or aeroportoSelecionado is None:
+            print("ALERT: selecione aeroporto válido.")
+            return menuManipulacao()
+        vaga = aeroportoSelecionado.buscarPlataformaDisponivel()
+        if vaga is None:
+            print(
+                "ALERT: capacidade máxima do aeroporto atingida. "
+                "adicione mais plataformas para comportar mais aeronaves."
+            )
             return menuManipulacao()
         aviao = menuLista(avioes, "aeronaves")
         if aviao is None:
             return menuManipulacao()
         centroSelecionado._controleVoo.adicionarAeronave(aviao)
+        vaga.ocupar(aviao)
         return menuManipulacao()
     elif mn == 3:
         if centroSelecionado is None:
@@ -236,13 +252,19 @@ def menuManipulacao():
         aviao = menuLista(avioes, "aeronaves")
         if aviao is None:
             return menuManipulacao()
+        platUtilizada = None
+        for plat in aeroportoSelecionado._plataformas:
+            if plataforma._aviao is aviao:
+                platUtilizada = plat
+                break
+        platUtilizada.liberar()
         centroSelecionado._controleVoo.removerAeronave(aviao)
         return menuManipulacao()
     elif mn == 4:
         if centroSelecionado is None:
             print("ALERT: primeiro selecione um aeroporto.")
             return menuManipulacao()
-        aviao = menuLista(centroSelecionado._controleVoo._aeronaves, "aviões")
+        aviao = menuLista(centroSelecionado._controleVoo.aeronaves, "aviões")
         if aviao is not None:
             aviaoSelecionado = aviao
         return menuManipulacao()
@@ -271,14 +293,11 @@ def menuManipulacao():
         if aviaoSelecionado is None:
             print("ALERT: primeiro selecione um avião.")
             return menuManipulacao()
-        aviaoSelecionado.removerObserver(
-            menuLista(controles, "observadores")
-        )
+        aviaoSelecionado.removerObserver(menuLista(controles, "observadores"))
         return menuManipulacao()
     else:
         print("ALERT: entrada inválida.")
-        menuManipulacao()
-    return menu()
+        return menuManipulacao()
 
 
 def menuEstados():
@@ -290,16 +309,35 @@ def menuEstados():
     print("[2] Informar Decolagem")
     print("[3] Solicitar Pouso")
     print("[4] Informar Pouso")
+    print("[5] Declarar Emergência")
     print("[0] Voltar")
     mn = input("---> ")
     if mn.isdigit():
         mn = int(mn)
     if mn == 0:
         return menuManipulacao()
+    elif mn == 1:
+        aviaoSelecionado.solicitarDecolagem()
+        centroSelecionado._controleVoo.solicitarDecolagem(aviaoSelecionado)
+        centroSelecionado.autorizarDecolagem(aviaoSelecionado)
+        return menuEstados()
+    elif mn == 2:
+        centroSelecionado.processarDecolagem(aviaoSelecionado)
+        return menuEstados()
+    elif mn == 3:
+        aviaoSelecionado.solicitarPouso()
+        centroSelecionado._controleVoo.solicitarPouso(aviaoSelecionado)
+        centroSelecionado.autorizarPouso(aviaoSelecionado)
+        return menuEstados()
+    elif mn == 4:
+        centroSelecionado.processarPouso(aviaoSelecionado)
+        return menuEstados()
+    elif mn == 5:
+        centroSelecionado.processarEmergencia(aviaoSelecionado)
+        return menuEstados()
     else:
         print("ALERT: entrada inválida.")
-        menuEstados()
-    return menuManipulacao()
+        return menuEstados()
 
 
 def menuCriacao():
@@ -311,16 +349,160 @@ def menuCriacao():
     print("[5] Criar Plataforma")
     print("[6] Criar Plano de Voo")
     print("[7] Criar Aviao")
+    print("[8] Remover Pista")
+    print("[9] Remover Plataforma")
     print("[0] Voltar")
     mn = input("---> ")
     if mn.isdigit():
         mn = int(mn)
     if mn == 0:
         return menu()
+    elif mn == 1:
+        nome = input("[INFO] NOME: ")
+        codigo = input("[INFO] CODIGO: ")
+        cidade = input("[INFO] CIDADE: ")
+        criar = input("[CRIAR] S/N: ")
+        if criar.lower() == "s":
+            aeroportos.append((nome, Aeroporto(nome, codigo, cidade)))
+        return menuCriacao()
+    elif mn == 2:
+        nome = input("[INFO] NOME: ")
+        criar = input("[CRIAR] S/N: ")
+        if criar.lower() == "s":
+            controles.append((nome, ControleVoo()))
+        return menuCriacao()
+    elif mn == 3:
+        nome = input("[INFO] NOME: ")
+        print("[INFO] SELECIONE O AEROPORTO: ")
+        aeroporto = menuLista(aeroportos, "aeroportos")
+        if aeroporto is None:
+            return menuCriacao()
+        print("[INFO] SELECIONE O CONTROLE DE VOO: ")
+        controle = menuLista(controles, "controles de voo")
+        if controle is None:
+            return menuCriacao()
+        criar = input("[CRIAR] S/N: ")
+        if criar.lower() == "s":
+            centros.append((nome, CentroOperacional(aeroporto, controle)))
+        return menuCriacao()
+    elif mn == 4:
+        print("[INFO] SELECIONE O AEROPORTO: ")
+        aeroporto = menuLista(aeroportos, "aeroportos")
+        if aeroporto is None:
+            return menuCriacao()
+        criar = input("[CRIAR] S/N: ")
+        if criar.lower() == "s":
+            aeroporto.adicionarPista(Pista(len(aeroporto._pistas)))
+        return menuCriacao()
+    elif mn == 5:
+        print("[INFO] SELECIONE O AEROPORTO: ")
+        aeroporto = menuLista(aeroportos, "aeroportos")
+        if aeroporto is None:
+            return menuCriacao()
+        criar = input("[CRIAR] S/N: ")
+        if criar.lower() == "s":
+            aeroporto.adicionarPlataforma(Plataforma(len(aeroporto._plataformas)))
+        return menuCriacao()
+    elif mn == 6:
+        origem = input("[INFO] ORIGEM: ")
+        destino = input("[INFO] DESTINO: ")
+        horarioPartida = input("[INFO] HORÁRIO DA PARTIDA(MM:HH): ")
+        horarioChegada = input("[INFO] HORÁRIO DA CHEGADA(MM:HH): ")
+        altitude = int(input("[INFO] ALTITUDE DE CRUZEIRO: "))
+        criar = input("[CRIAR] S/N: ")
+        if criar.lower() == "s":
+            planos.append(
+                (
+                    destino,
+                    PlanoVoo(origem, destino, horarioPartida, horarioChegada, altitude),
+                )
+            )
+        return menuCriacao()
+    elif mn == 7:
+        tipo = input("[INFO] TIPO DO AVIAO(COMERCIAL, PRIVADO, TRANSPORTE): ")
+        identificador = input("[INFO] IDENTIFICADOR: ")
+        modelo = input("[INFO] MODELO: ")
+        if tipo.upper() == "COMERCIAL":
+            companhia = input("[INFO] COMPANHIA AÉREA: ")
+            nroPassageiro = int(input("[INFO] NÚMERO DE PASSAGEIROS: "))
+            criar = input("[CRIAR] S/N: ")
+            if criar.lower() == "s":
+                avioes.append(
+                    (
+                        identificador,
+                        FactoryAviao.criarAviao(
+                            tipo,
+                            identificador=identificador,
+                            modelo=modelo,
+                            companhiaAerea=companhia,
+                            numeroPassageiros=nroPassageiro,
+                        ),
+                    )
+                )
+            return menuCriacao()
+        if tipo.upper() == "PRIVADO":
+            proprietario = input("[INFO] PROPRIETÁRIO: ")
+            nroPassageiro = int(input("[INFO] NÚMERO DE PASSAGEIROS: "))
+            criar = input("[CRIAR] S/N: ")
+            if criar.lower() == "s":
+                avioes.append(
+                    (
+                        identificador,
+                        FactoryAviao.criarAviao(
+                            tipo,
+                            identificador=identificador,
+                            modelo=modelo,
+                            proprietario=proprietario,
+                            numeroPassageiros=nroPassageiro,
+                        ),
+                    )
+                )
+            return menuCriacao()
+        if tipo.upper() == "TRANSPORTE":
+            tipoCarga = input("[INFO] TIPO DA CARGA: ")
+            pesoCarga = float(input("[INFO] PESO DA CARGA: "))
+            criar = input("[CRIAR] S/N: ")
+            if criar.lower() == "s":
+                avioes.append(
+                    (
+                        identificador,
+                        FactoryAviao.criarAviao(
+                            tipo,
+                            identificador=identificador,
+                            modelo=modelo,
+                            tipoCarga=tipoCarga,
+                            pesoCarga=pesoCarga,
+                        ),
+                    )
+                )
+        return menuCriacao()
+    elif mn == 8:
+        print("[INFO] SELECIONE O AEROPORTO: ")
+        aeroporto = menuLista(aeroportos, "aeroportos")
+        if aeroporto is None:
+            return menuCriacao()
+        pista = menuLista(aeroporto._pistas, "pistas")
+        if pista is None:
+            return menuCriacao()
+        remover = input("[REMOVER] S/N: ")
+        if remover.lower() == "s":
+            aeroporto.removerPista(pista)
+        return menuCriacao()
+    elif mn == 9:
+        print("[INFO] SELECIONE O AEROPORTO: ")
+        aeroporto = menuLista(aeroportos, "aeroportos")
+        if aeroporto is None:
+            return menuCriacao()
+        plataforma = menuLista(aeroporto._plataformas, "plataformas")
+        if plataforma is None:
+            return menuCriacao()
+        remover = input("[REMOVER] S/N: ")
+        if remover.lower() == "s":
+            aeroporto.removerPlataforma(plataforma)
+        return menuCriacao()
     else:
         print("ALERT: entrada inválida.")
-        menuCriacao()
-    return menu()
+        return menuCriacao()
 
 
 def main():
